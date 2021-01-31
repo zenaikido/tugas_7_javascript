191bb71b6bb0d0ecf22cf8d519215ace54a18778
Rauf <zen.star12@yahoo.com>
2021-01-31 18:07:13 +0700

    Tugas 7 Javascript Niomic

---
+++ tugas7.js
@@ -0,0 +1,10 @@
+var tinggi = [150,160,170,180,190,200,210,220,230,155];
+
+for(i=0;i<tinggi.length;i++){
+    console.log(tinggi[i])
+}
+console.log("")
+
+for(let x of tinggi){
+    console.log(x)
+}
